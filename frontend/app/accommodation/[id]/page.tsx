3c0f9e1ef5568ac345d5fe63f4af50a833f3d4a6
axel <axel.f.reyes.a@gmail.com>
2025-05-07 16:57:15 -0600

    Titulos de Componentes

--- frontend/app/accommodation/[id]/page.tsx
+++ frontend/app/accommodation/[id]/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Accommodation } from '@/components/Accommodation/Accommodation';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'Plan Nutricional',
+  description: 'Revisa tu plan presentado por la IA.',
+};
 const AccommodationPage = async ({
   params,
 }: {
--- frontend/app/accommodations-support/page.tsx
+++ frontend/app/accommodations-support/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { AccommodationsSupport } from '@/components/AccommodationsSupport/AccommodationsSupport';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'Especialista en Nutrición',
+  description: 'Contacta con un especialisa si requires atencion en un area dedicada.',
+};
 const AccommodationsSupportPage = () => {
   return <AccommodationsSupport />;
 };
